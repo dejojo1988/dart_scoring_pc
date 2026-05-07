91c4ee47f5ad133de26dd18778ade30feb79908b
Jochen <dejojo1988@gmail.com>
2026-05-07 09:47:47 +0200

    Bump version to 1.0.3.1 beta

--- lib/app_version.dart
+++ lib/app_version.dart
@@ -1,13 +1,16 @@
 class AppVersion {
   AppVersion._();
 
-  static const String number = '1.0.3.0';
-  static const String channel = 'BETA';
+  static const String number = '1.0.3.1';
 
-  static const String visibleLabel = 'VERSION 1.0.3.0 · BETA';
+  static const String channel = 'beta';
+
+  static const String visibleLabel = 'VERSION 1.0.3.1 · BETA';
+
   static const String homeFooterLabel =
-      'Dart Scoring PC · Version 1.0.3.0 · BETA';
+      'Dart Scoring PC · Version 1.0.3.1 · BETA';
 
-  static const String windowTitle = 'Dart Scoring PC 1.0.3.0 BETA';
-  static const String installerVersion = '1.0.3.0-beta';
+  static const String windowTitle = 'Dart Scoring PC 1.0.3.1 BETA';
+
+  static const String installerVersion = '1.0.3.1-beta';
 }
