e60346ed4a37c79f3b0a64ff8cc0d09564a726de
Jochen <dejojo1988@gmail.com>
2026-05-08 22:09:21 +0200

    Prepare beta update 1.0.3.4

--- lib/app_version.dart
+++ lib/app_version.dart
@@ -1,16 +1,16 @@
 class AppVersion {
   AppVersion._();
 
-  static const String number = '1.0.3.3';
+  static const String number = '1.0.3.4';
 
   static const String channel = 'beta';
 
-  static const String visibleLabel = 'VERSION 1.0.3.3 · BETA';
+  static const String visibleLabel = 'VERSION 1.0.3.4 · BETA';
 
   static const String homeFooterLabel =
-      'Dart Scoring PC · Version 1.0.3.3 · BETA';
+      'Dart Scoring PC · Version 1.0.3.4 · BETA';
 
-  static const String windowTitle = 'Dart Scoring PC 1.0.3.3 BETA';
+  static const String windowTitle = 'Dart Scoring PC 1.0.3.4 BETA';
 
-  static const String installerVersion = '1.0.3.3-beta';
+  static const String installerVersion = '1.0.3.4-beta';
 }
